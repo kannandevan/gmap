54991d3b2c7ea496f6c60b2a11c46d296be25cdc
DEVANAND G <kannandevan007@gmail.com>
2020-11-27 19:32:52 +0530

    api added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
