4ed1bda2327a718f94f0eb7048bb46160a460808
DEVANAND G <kannandevan007@gmail.com>
2020-12-01 02:08:06 +0530

    Lat lng assigned TASK FINISHED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, Pipe } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component  } from '@angular/core';
+import {} from 'googlemaps';
 
 
 import data from '../assets/data.json';
@@ -8,24 +11,61 @@ import data from '../assets/data.json';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
+
+  
   
   title = 'gmap';
   statesArray:any = data;
   selectedState:any;
+  selectedDistrict:any;
   state:any;
   districts:any;
+  number:any;
   index:any;
-   constructor() {
+  lat: number = 9.5917539596557617;
+  lng: number = 76.531906127929687;
+  baseUrl: string = 'http://dev.virtualearth.net/REST/v1/Locations/';
+  urlAdd:string = '?o=json&key=';
+  key : string ='AohIKn07G_cGHbkr9mqaeVTe47o47k-T6W_VfK_MZcwNZzGKOn8Jcv6IuD-btkZc';
+  coordinates :object;
+
+
+   constructor(private http: HttpClient) {
     
    }
+   
+
   public stateChanged(state){
   this.selectedState = state.target.value;
-  this.index = state.currentTarget.options.selectedIndex;
+  this.number = state.currentTarget.options.selectedIndex;
+  this.index = this.number-1
   this.state = this.statesArray.states[this.index];
   this.districts=this.statesArray.states[this.index].districts;
-  console.log(this.districts);
-
   }
+
+
+
+  public districtSelected(district){
+    console.log(district.target.value);
+    this.getLatlngJson(district.target.value);
+  }
+
+ getLatlngJson(district : string){
+   return this.http.get(this.baseUrl + district + this.urlAdd +this.key ).subscribe(data =>{
+    this.coordinates =data;
+    
+    console.log(this.coordinates);
+     this.lat = JSON.parse(JSON.stringify(this.coordinates))['resourceSets'][0]['resources'][0]['point']['coordinates'][0];
+     this.lng = JSON.parse(JSON.stringify(this.coordinates))['resourceSets'][0]['resources'][0]['point']['coordinates'][1];
+   });
+  
+}
+
+  
+  
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,10 +16,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NgbModule,
     RouterModule,
     ReactiveFormsModule,
-    FormsModule
-    
+    FormsModule,
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA5n-PmGuIDFOcZimTwP7HIYdvhEfAPSns'
+    }),
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
